eea860e1e627bc4eae16edc28b22444a20ce183d
joshseyda <josh.seyda@gmail.com>
2018-07-19 19:49:47 -0400

    inception

--- wrangle.sql
+++ wrangle.sql
@@ -1,5 +1,13 @@
-CREATE TABLE zip_codes 
+CREATE TABLE precinct 
 (ZIP char(5), LATITUDE double precision, LONGITUDE double precision, 
 CITY varchar, STATE char(2), COUNTY varchar, ZIP_CLASS varchar);
 
+street city state/zip precinct_id
+COPY zip_codes FROM '/path/to/csv/ZIP_CODES.txt' WITH (FORMAT csv);
+
+CREATE TABLE citizens
+(ZIP char(5), LATITUDE double precision, LONGITUDE double precision, 
+CITY varchar, STATE char(2), COUNTY varchar, ZIP_CLASS varchar);
+
+street apt city state zip precinct_id 
 COPY zip_codes FROM '/path/to/csv/ZIP_CODES.txt' WITH (FORMAT csv);
