107557e05591251df84736d5a9ba2aa0b699bebf
joshseyda <josh.seyda@gmail.com>
2018-07-19 20:47:05 -0400

    populating two tables with CSV files

--- wrangle.sql
+++ wrangle.sql
@@ -1,25 +1,25 @@
-CREATE DATABASE democracy_works
-DROP TABLE IF EXISTS precinct CASCADE;
+CREATE DATABASE democracy_works;
+\connect democracy_works
+
+DROP TABLE IF EXISTS precincts CASCADE;
 DROP TABLE IF EXISTS citizens CASCADE;
 
-CREATE TABLE precinct (
-    id integer NOT NULL,
+CREATE TABLE precincts (
     street text NOT NULL,
     city text NOT NULL,
     state_zip text NOT NULL,
-    precinct_id text NOT NULL
+    country text,
+    precinct_id text 
 );
 
 CREATE TABLE citizens (
-    id integer NOT NULL,
     street text NOT NULL,
-    apt text NOT NULL,
+    apt text ,
     city text NOT NULL,
     state text NOT NULL,
     zipcode text NOT NULL,
     precinct_id text NOT NULL
 );
 
-COPY precinct FROM 'precinct_polling_list.csv' WITH (FORMAT csv);
-
-COPY citizens FROM 'addresses.csv' WITH (FORMAT csv);
+COPY precincts(street, city, state_zip, country, precinct_id) FROM '/Users/joshuaseyda/Documents/wdi1/democracy_works/democracy_works_01/precinct_polling_list.csv' DELIMITER ',' CSV HEADER; 
+COPY citizens(street, apt, city, state, zipcode, precinct_id) FROM '/Users/joshuaseyda/Documents/wdi1/democracy_works/democracy_works_01/addresses.csv' DELIMITER ',' CSV HEADER;  
