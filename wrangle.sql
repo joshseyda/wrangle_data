57caa7f129b58a2bf4117beb294887b5263d4172
joshseyda <josh.seyda@gmail.com>
2018-07-22 16:51:09 -0400

    Tables are joined into new table sorted by voting precinct

--- wrangle.sql
+++ wrangle.sql
@@ -3,6 +3,7 @@ CREATE DATABASE democracy_works;
 
 DROP TABLE IF EXISTS precincts CASCADE;
 DROP TABLE IF EXISTS citizens CASCADE;
+DROP TABLE IF EXISTS new_records CASCADE;
 
 CREATE TABLE precincts (
     id serial NOT NULL,
@@ -25,3 +26,10 @@ CREATE TABLE citizens (
 
 COPY precincts(street, city, state_zip, country, precinct_id) FROM '/Users/joshuaseyda/Documents/wdi1/democracy_works/democracy_works_01/precinct_polling_list_sort.csv' DELIMITER ',' CSV HEADER; 
 COPY citizens(street, apt, city, state, zipcode, precinct_id) FROM '/Users/joshuaseyda/Documents/wdi1/democracy_works/democracy_works_01/addresses.csv' DELIMITER ',' CSV HEADER;  
+
+CREATE TABLE new_records AS
+SELECT precincts.street AS "Voting_Street", precincts.city AS "Voting_City", precincts.state_zip AS "Precinct_ZIP", precincts.precinct_id, citizens.street AS "Citizen_Street", citizens.apt AS "Citizen_APT", citizens.city AS "Citizen_City", citizens.state AS "Citizen_State", citizens.zipcode  AS "Citizen_ZIP" FROM precincts
+INNER JOIN citizens ON precincts.precinct_id = citizens.precinct_id;
+-- UNION ALL 
+-- SELECT citizens.street, citizens.apt, citizens.city, citizens.state, citizens.zipcode, citizens.precinct_id FROM citizens
+-- FULL OUTER JOIN citizens ON precincts.precinct_id = citizens.precinct_id;
