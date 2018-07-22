2e4aa08c8a284c82040149bc9b02da8bbd199d9d
joshseyda <josh.seyda@gmail.com>
2018-07-22 16:00:42 -0400

    zip code, country, and precinct id are all reformatted

--- sort.rb
+++ sort.rb
@@ -6,8 +6,9 @@ puts " Reading data from  : #{src_dir}"
 puts " Writing data to    : #{dst_dir}"
 
 state_codes = {
+  "ARI": "004",
   "CAL": "006",
-  "CT": "009",
+  "CON": "009",
   "FLO": "012",
   "GEO": "013",
   "ILL": "017",
@@ -23,35 +24,74 @@ state_codes = {
 
 #create a new file 
 csv_out = File.open(dst_dir, 'wb')
-
+csv_out << "Street,City,State/ZIP,Country,Precinct
+"
 #read from existing file
 CSV.foreach(src_dir , headers: true) do |row|
   puts row.inspect
   # the below conditional checks the values in the CSV rows for the format XXX-XXX and allocates the proper values accordingly
-  if row["Precinct"] == nil
-    if /((...)\-(...))/.match(row["Country"])
-      puts "reassigning 3 to 4"
-      row["Precinct"] = row["Country"]
-      puts row
-      # /(...)\-/.match(row["Precinct"])
-      string_val = row["Precinct"].to_s
-      split_string = string_val.split("-")
-      puts split_string
-      new_val = state_codes[split_string[0].to_sym]
-      p new_val
+  # if /((\D{2})\s(\d{5}))/.match(row["Street"])
+  #   row["State/ZIP"] = /((\D{2})\s(\d{5}))/.match(row["Street"]).to_s
+  #   puts row["State/ZIP"]
+  # end
 
+  if row["Precinct"] == nil
+
+    if /((...)\-(...))/.match(row["Country"])
+      puts "reassigning col 3 to col 4"
+      string_val = row["Country"].to_s
+      split_string = string_val.split("-")
+      new_val = state_codes[split_string[0].to_sym]
+      row["Precinct"] = "#{new_val}-#{split_string[1]}"
+      row["Country"] = "USA"
+      if /((\D{2})\s(\d{5}))/.match(row["Street"])
+        row["State/ZIP"] = /((\D{2})\s(\d{5}))/.match(row["Street"]).to_s
+        puts row["State/ZIP"]
+      end
+      puts row.inspect
       csv_out << row
+
     elsif /((...)\-(...))/.match(row["State/ZIP"])
-      puts "reassigning 2 to 4"
-      row["Precinct"] = row["State/ZIP"] 
-      puts row
+      puts "reassigning col 2 to col 4"
+      string_val = row["State/ZIP"].to_s
+      split_string = string_val.split("-")
+      new_val = state_codes[split_string[0].to_sym]
+      row["Precinct"] = "#{new_val}-#{split_string[1]}"
+      row["Country"] = "USA"
+      if /((\D{2})\s(\d{5}))/.match(row["Street"])
+        row["State/ZIP"] = /((\D{2})\s(\d{5}))/.match(row["Street"]).to_s
+        puts row["State/ZIP"]
+      end
+      puts row.inspect
       csv_out << row
+
     else
       puts "I can not find the precinct ID in columns 2, 3, or 4!"
     end
+
   else
     # this command pushes the row to a new document which our database will be built off of
+
+    if /(--)/.match(row["Precinct"])
+      sub_str_val = row["Precinct"].to_s
+      split_sub= sub_str_val.split("--")
+      new_sub_str = "#{split_sub[0]}-0#{split_sub[1]}"
+      row["Precinct"] = new_sub_str
+    end  
+
+    if /((\D{2})\s(\d{5}))/.match(row["Street"])
+      row["State/ZIP"] = /((\D{2})\s(\d{5}))/.match(row["Street"]).to_s
+      puts row["State/ZIP"]
+    end
+
+    row["Country"] = "USA"
+    string_val = row["Precinct"].to_s
+    split_string = string_val.split("-")
+    new_val = state_codes[split_string[0].to_sym]
+    row["Precinct"] = "#{new_val}-#{split_string[1]}"
+    puts row.inspect
     csv_out << row
+
   end
  puts "*"
  puts "*"
@@ -61,6 +101,7 @@ CSV.foreach(src_dir , headers: true) do |row|
  puts "*"
  puts "*"
  puts "*"
+
 end
 
 # close the file 
